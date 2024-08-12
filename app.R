9170f3803a5c1b86b7203f31f895b00f8ddcece9
joshwlivingston <joshwlivingston@gmail.com>
2024-08-12 14:27:10 -0700

    cat did not work, gotta use logger

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 generate_story <- function(noun, verb, adjective, adverb) {
-  cat("Creating story:", noun)
+  logger::log_info("Creating story:", noun)
   glue::glue("
     Once upon a time, there was a {adjective} {noun} who loved to
     {verb} {adverb}. It was the funniest thing ever!
